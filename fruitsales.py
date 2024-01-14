4da6f1b287948fbc7e3f14fdc20c11a3f83a3df3
Kayla <you@example.com>
2024-01-13 21:22:20 -0500

    Fruit Sales

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
 # add your code here
+# Creating Data Frame
+import pandas as pd
+ 
+df = pd.DataFrame({'Apples': ['35', '41'],
+                   'Bananas': ['21', '34']}, index = ['2017 Sales', '2018 Sales'])
+
+print(df)
+
+df.to_csv("fruit.csv", index = True)
